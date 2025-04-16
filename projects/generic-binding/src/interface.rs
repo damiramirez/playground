d52c494d6b292be92b8ae276bcdad7fd9f701994
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-04-16 10:10:03 -0300

    feat: working with binding and generics

--- projects/generic-binding/src/interface.rs
+++ projects/generic-binding/src/interface.rs
@@ -1,23 +1,98 @@
-use alloy::primitives::Bytes;
+use alloy::contract::ContractInstance;
 
-// Estructura genérica en el SDK para representar una tarea.
+pub type GenericContractInstance = ContractInstance<
+    (),
+    alloy::providers::fillers::FillProvider<
+        alloy::providers::fillers::JoinFill<
+            alloy::providers::fillers::JoinFill<
+                alloy::providers::Identity,
+                alloy::providers::fillers::JoinFill<
+                    alloy::providers::fillers::GasFiller,
+                    alloy::providers::fillers::JoinFill<
+                        alloy::providers::fillers::BlobGasFiller,
+                        alloy::providers::fillers::JoinFill<
+                            alloy::providers::fillers::NonceFiller,
+                            alloy::providers::fillers::ChainIdFiller,
+                        >,
+                    >,
+                >,
+            >,
+            alloy::providers::fillers::WalletFiller<alloy::network::EthereumWallet>,
+        >,
+        alloy::providers::RootProvider,
+    >,
+>;
+
+#[allow(non_snake_case)]
+#[derive(Debug)]
+pub struct TaskResponse<OUTPUT> {
+    #[allow(missing_docs)]
+    pub referenceTaskIndex: u32,
+    #[allow(missing_docs)]
+    pub response: OUTPUT,
+}
+
 #[allow(non_snake_case)]
 #[derive(Debug)]
 pub struct Task<INPUT> {
+    #[allow(missing_docs)]
     pub input: INPUT,
+    #[allow(missing_docs)]
     pub taskCreatedBlock: u32,
-    pub quorumNumbers: Bytes,
+    #[allow(missing_docs)]
+    pub quorumNumbers: alloy::sol_types::private::Bytes,
+    #[allow(missing_docs)]
     pub quorumThresholdPercentage: u32,
 }
 
-// Trait para definir el contrato de binding de la tarea.
-// Este trait permite transformar la representación genérica en la
-// estructura específica requerida (que vendría del binding del contrato).
-pub trait TaskBindingContract {
-    type Input;
-    type Task;
+/// This is the documentation for the `NonSignerStakesAndSignature` struct.
+#[allow(non_camel_case_types, non_snake_case, clippy::pub_underscore_fields)]
+#[derive(Debug, Clone)]
+pub struct NonSignerStakesAndSignature<G1Point, G2Point>
+where
+    G1Point: alloy::sol_types::SolType + Clone,
+    G2Point: alloy::sol_types::SolType + Clone,
+{
+    #[allow(missing_docs)]
+    pub nonSignerQuorumBitmapIndices: alloy::sol_types::private::Vec<u32>,
+    #[allow(missing_docs)]
+    pub nonSignerPubkeys: alloy::sol_types::private::Vec<G1Point>,
+    #[allow(missing_docs)]
+    pub quorumApks: alloy::sol_types::private::Vec<G1Point>,
+    #[allow(missing_docs)]
+    pub apkG2: G2Point, // as alloy::sol_types::SolType,
+    #[allow(missing_docs)]
+    pub sigma: G1Point, // as alloy::sol_types::SolType,
+    #[allow(missing_docs)]
+    pub quorumApkIndices: alloy::sol_types::private::Vec<u32>,
+    #[allow(missing_docs)]
+    pub totalStakeIndices: alloy::sol_types::private::Vec<u32>,
+    #[allow(missing_docs)]
+    pub nonSignerStakeIndices: alloy::sol_types::private::Vec<alloy::sol_types::private::Vec<u32>>,
+}
 
-    /// Convierte la tarea (con input genérico) en la representación de binding
-    /// esperada por el contrato.
-    fn create_task(&self) -> Self::Task;
+pub trait TaskManagerContract {
+    /// Type for task indices
+    //type Index;
+
+    /// Type for inputs of each task
+    type Input;
+
+    /// Type for outputs of each task
+    type Output;
+
+    /// G1Point type with required trait bounds
+    type G1Point: alloy::sol_types::SolType + Clone;
+
+    /// G2Point type with required trait bounds
+    type G2Point: alloy::sol_types::SolType + Clone;
+
+    fn create_new_task(&self, task: Task<Self::Input>) -> Task<Self::Input>;
+
+    fn respond_to_task(
+        &self,
+        task: Task<Self::Input>,
+        response: TaskResponse<Self::Output>,
+        non_signer_stakes_and_signature: NonSignerStakesAndSignature<Self::G1Point, Self::G2Point>,
+    );
 }
--- projects/generic-binding/src/is.rs
+++ projects/generic-binding/src/is.rs
@@ -1,25 +1,120 @@
-use alloy::primitives::U256;
-
-use crate::{
-    bindings::incrediblesquaringtaskmanager::IIncredibleSquaringTaskManager,
-    interface::{Task, TaskBindingContract},
+use alloy::{
+    network::{Ethereum, Network},
+    primitives::U256,
+    providers::Provider,
+    transports::Transport,
 };
 
-// Implementación del trait para la estructura genérica Task<INPUT>.
-// Se asume que el tipo INPUT puede convertirse en U256, que es lo que espera el binding.
-impl<INPUT> TaskBindingContract for Task<INPUT>
-where
-    INPUT: Into<U256> + Clone,
-{
-    type Input = INPUT;
-    type Task = IIncredibleSquaringTaskManager::Task;
+use crate::{
+    bindings::incrediblesquaringtaskmanager::{
+        IBLSSignatureCheckerTypes::NonSignerStakesAndSignature,
+        IIncredibleSquaringTaskManager::{
+            Task as TaskContract, TaskResponse as TaskResponseContract,
+        },
+        IncredibleSquaringTaskManager::IncredibleSquaringTaskManagerInstance,
+        BN254::{G1Point, G2Point},
+    },
+    interface::{Task, TaskManagerContract, TaskResponse},
+};
 
-    fn create_task(&self) -> Self::Task {
-        IIncredibleSquaringTaskManager::Task {
-            numberToBeSquared: self.input.clone().into(),
-            taskCreatedBlock: self.taskCreatedBlock,
-            quorumNumbers: self.quorumNumbers.clone(),
-            quorumThresholdPercentage: self.quorumThresholdPercentage,
-        }
+type ContractInstance = IncredibleSquaringTaskManagerInstance<
+    (),
+    alloy::providers::fillers::FillProvider<
+        alloy::providers::fillers::JoinFill<
+            alloy::providers::fillers::JoinFill<
+                alloy::providers::Identity,
+                alloy::providers::fillers::JoinFill<
+                    alloy::providers::fillers::GasFiller,
+                    alloy::providers::fillers::JoinFill<
+                        alloy::providers::fillers::BlobGasFiller,
+                        alloy::providers::fillers::JoinFill<
+                            alloy::providers::fillers::NonceFiller,
+                            alloy::providers::fillers::ChainIdFiller,
+                        >,
+                    >,
+                >,
+            >,
+            alloy::providers::fillers::WalletFiller<alloy::network::EthereumWallet>,
+        >,
+        alloy::providers::RootProvider,
+    >,
+>;
+
+struct ContractImpl {
+    task_manager: ContractInstance,
+}
+
+impl ContractImpl {
+    fn new(task_manager: ContractInstance) -> Self {
+        task_manager.address();
+        Self { task_manager }
+    }
+}
+
+impl TaskManagerContract for ContractImpl {
+    type Input = U256;
+    type Output = U256;
+    type G1Point = G1Point;
+    type G2Point = G2Point;
+
+    fn create_new_task(&self, task: Task<Self::Input>) -> Task<Self::Input> {
+        task
+    }
+
+    fn respond_to_task(
+        &self,
+        task: Task<Self::Input>,
+        response: TaskResponse<Self::Output>,
+        non_signer_stakes_and_signature: crate::interface::NonSignerStakesAndSignature<
+            Self::G1Point,
+            Self::G2Point,
+        >,
+    ) {
+        // self.task_manager
+        //     .respondToTask(task, response, non_signer_stakes_and_signature)
+        //     .send();
+
+        let contract_task = TaskContract {
+            numberToBeSquared: task.input,
+            taskCreatedBlock: task.taskCreatedBlock,
+            quorumNumbers: task.quorumNumbers,
+            quorumThresholdPercentage: task.quorumThresholdPercentage,
+        };
+
+        let contract_task_response = TaskResponseContract {
+            referenceTaskIndex: response.referenceTaskIndex,
+            numberSquared: response.response,
+        };
+
+        let non_signer_contract = NonSignerStakesAndSignature {
+            nonSignerQuorumBitmapIndices: non_signer_stakes_and_signature
+                .nonSignerQuorumBitmapIndices,
+            nonSignerPubkeys: non_signer_stakes_and_signature.nonSignerPubkeys,
+            quorumApks: non_signer_stakes_and_signature.quorumApks,
+            apkG2: non_signer_stakes_and_signature.apkG2,
+            sigma: non_signer_stakes_and_signature.sigma,
+            quorumApkIndices: non_signer_stakes_and_signature.quorumApkIndices,
+            totalStakeIndices: non_signer_stakes_and_signature.totalStakeIndices,
+            nonSignerStakeIndices: non_signer_stakes_and_signature.nonSignerStakeIndices,
+        };
+
+        self.task_manager
+            .respondToTask(contract_task, contract_task_response, non_signer_contract)
+            .send();
+    }
+}
+
+pub struct TaskManagerAdapter<T, P, N = Ethereum> {
+    pub contract: IncredibleSquaringTaskManagerInstance<T, P, N>,
+}
+
+impl<T, P, N> TaskManagerAdapter<T, P, N>
+where
+    T: Transport + Clone,
+    P: Provider<N>,
+    N: Network,
+{
+    fn respond_to_task(&self) {
+        &self.contract;
     }
 }
