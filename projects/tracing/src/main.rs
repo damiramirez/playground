bfb885987ad039a64c2e58a687b3777fe5e8ea90
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-06-12 12:06:07 -0300

    feat: check debug with key-value

--- projects/tracing/src/main.rs
+++ projects/tracing/src/main.rs
@@ -1,19 +1,32 @@
-use tracing::{info, instrument};
+use tracing::{debug, info, instrument, level_filters::LevelFilter};
 
-#[tokio::main]
-async fn main() {
+fn main() {
     tracing_subscriber::fmt()
-        .with_target(true)
-        .with_file(true)
-        .with_line_number(true)
+        .with_max_level(LevelFilter::DEBUG)
         .init();
 
-    handle_new_signature(42).await;
+    handle_new_signature(42);
+    handle_new_task(42);
+
+    let len = 10;
+    let i = 1;
+    let to_block = 100;
+
+    debug!("numTransactionLogs: {len}, fromBlock: {i}, toBlock: {to_block}");
+
+    debug!(
+        num_transaction_logs = len,
+        from_block = i,
+        to_block = to_block,
+    );
 }
 
 // #[instrument] - adds context to the log
-#[instrument]
-async fn handle_new_signature(task_index: u32) {
-    info!("Using instrument in handle_new_signature. Skipping all");
-    info!("Processing new signature: {task_index}");
+#[instrument(skip_all)]
+fn handle_new_signature(task_index: u32) {
+    info!("(USING INSTRUMENT SKIP ALL) Processing new signature: {task_index}");
+}
+
+fn handle_new_task(task_index: u32) {
+    info!(target: "handle_new_task", "(USING TARGET) Processing new task: {task_index}");
 }
